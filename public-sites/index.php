fab5ceff5743d9d370c29fec4edb4ef4255c601d
vietnguyen-dev <vietnguyent22@gmail.com>
2024-04-17 22:34:17 -0700

    removed public php websites, going to do it all in react

--- public-sites/index.php
+++ public-sites/index.php
@@ -1,10 +1,13 @@
-<html>
-<head>
-	<title> Rankings </title>
-</head>
-<body>
-	<?php 
-		echo "hello world!"
-	?>
-</body>
-</html>	
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+   		<title>Bootstrap demo</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+  <body>
+	<nav>
+	</nav>
+  </body>
+</html>
