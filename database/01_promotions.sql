d48cc8c314ab2c3038daf39db6539fd911e7e524
vietnguyen-dev <vietnguyent22@gmail.com>
2024-04-17 19:55:35 -0700

    adding new field to promotions table

--- database/01_promotions.sql
+++ database/01_promotions.sql
@@ -2,6 +2,7 @@ CREATE TABLE promotions (
     id SERIAL PRIMARY KEY,
     pro_code VARCHAR(16) NOT NULL,
     name VARCHAR(256) NOT NULL,
+    website_link VARCHAR,
     date_created DATE DEFAULT CURRENT_DATE,
     date_updated DATE,
     date_deleted DATE
