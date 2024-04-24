58f9ab8698290421858dbdf3d4fb0f52d985cb40
vietnguyen-dev <vietnguyent22@gmail.com>
2024-04-23 21:08:12 -0700

    working promotions route

--- server/main.go
+++ server/main.go
@@ -1,25 +1,11 @@
 package main
 
-import (
-    "fmt"
-    "net/http"
+import ( 
+	"net/http"
+	"server/routes"
 )
 
-type promotions struct {
-	id int;
-	pro_code string;
-	name string;
-	how_many_ranked int;
-	website_link string;
-}
-
-// handler is a typical HTTP request-response handler in Go; details later
-func handler(w http.ResponseWriter, r *http.Request) {
-    // Fprintf formats the string to a writer
-    fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-    http.HandleFunc("/promotions", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/promotions/{id}", routes.Promotions)
+	http.ListenAndServe(":8080", nil)
 }
