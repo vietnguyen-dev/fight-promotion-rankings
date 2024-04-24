a59f22ba290c9bb45ff38f3a9e60a4c720a4618d
vietnguyen-dev <vietnguyent22@gmail.com>
2024-04-24 10:07:16 -0700

    fight results route

--- server/main.go
+++ server/main.go
@@ -12,5 +12,6 @@ func main() {
 	http.HandleFunc("/events/{promotions_id}", routes.Events)
 	http.HandleFunc("/weight-classes/{promotions_id}", routes.WeightClasses)
 	http.HandleFunc("/fights/{events_id}", routes.Fights)
+	http.HandleFunc("/fight-results/{fight_id}", routes.FightResults)
 	http.ListenAndServe(":8080", nil)
 }
