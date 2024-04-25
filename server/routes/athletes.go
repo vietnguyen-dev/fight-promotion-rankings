117fd739a05b0b023a1577bcad960d303093ea22
vietnguyen-dev <vietnguyent22@gmail.com>
2024-04-25 16:47:31 -0700

    fixed logic for athletes route

--- server/routes/athletes.go
+++ server/routes/athletes.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"	
 	"time"
+	"strings"
 	"server/utils"
 )
 
@@ -27,83 +28,104 @@ type vw_athletes struct {
 	S3Url 		 string `json:"s3_url"`;
 }
 
-func Athletes(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-		case "GET":
-			// connect to the database
-			db := utils.PgConnect()
-			defer db.Close()
-		
-			//query the promotions view
-			id := r.PathValue("promotions_id")
-			rows, err := db.Query("SELECT * FROM vw_athletes WHERE promotion_id = $1;", id)
-			if err != nil {
-				fmt.Println(err)
-			}
-			defer rows.Close()
+func GetAthletes(w http.ResponseWriter, r *http.Request) {
+	// connect to the database
+	db := utils.PgConnect()
+	defer db.Close()
+	
+	//query the promotions view
+	id := strings.TrimPrefix(r.URL.Path, "/athletes/") 	
+	promotion_id := r.URL.Query().Get("promotion_id")
+	fmt.Println("this is the id", promotion_id)
+	query := "SELECT * FROM vw_athletes"
+	args := []interface{}{}
 
-			//turn the data into proper json
-			var athletes []vw_athletes
-			for rows.Next() {
-				var athlete vw_athletes
-				var NickName sql.NullString
-				var GymName sql.NullString
-				var Height sql.NullString
-				var Location sql.NullString
-				var Reach sql.NullInt64
-				var Dob sql.NullTime
-				var ActiveStatus sql.NullBool
-				var InstagramLink sql.NullString
-				var TwitterLink sql.NullString
-				var FacebookLink sql.NullString
-				var S3Url sql.NullString
-				if err := rows.Scan(
-					&athlete.Id,
-					&athlete.FirstName,
-					&athlete.LastName,
-					&NickName,
-					&GymName,
-					&Location,
-					&Height,
-					&Reach,
-					&Dob,
-					&ActiveStatus,
-					&athlete.PromotionId,
-					&InstagramLink,
-					&TwitterLink,
-					&FacebookLink,
-					&S3Url,
-				); err != nil {
-					http.Error(w, "Data extraction error", http.StatusInternalServerError)
-					fmt.Println(err)
-					return
-				}
-				athlete.NickName = utils.NullStringToString(NickName) 
-				athlete.GymName = utils.NullStringToString(GymName)
-				athlete.Height = utils.NullStringToString(Height)
-				athlete.Location = utils.NullStringToString(Location)
-				athlete.Reach = utils.NullInt64ToInt(Reach)
-				athlete.Dob = utils.NullTimeToTime(Dob)
-				athlete.ActiveStatus= utils.NullBoolToBool(ActiveStatus) 
-				athlete.InstagramLink = utils.NullStringToString(InstagramLink)
-				athlete.TwitterLink = utils.NullStringToString(TwitterLink)
-				athlete.FacebookLink = utils.NullStringToString(FacebookLink)
-				athlete.S3Url = utils.NullStringToString(S3Url)
-				athletes = append(athletes, athlete)
-			}
+	if len(id) > 0 {
+		if len(args) == 0 {
+			query += " WHERE id = $1"
+		}
+		args = append(args, id)
+	}
 
-			//return data
-			jsonData, err := json.Marshal(athletes)
-			if err != nil {
-				http.Error(w, "JSON serialization failed", http.StatusInternalServerError)
+	if len(promotion_id) > 0 {
+		if len(args) == 0 {
+			query += " WHERE promotion_id = $1"
+		} else {
+			query += " AND promotion_id = $2"
+		}
+		args = append(args, promotion_id)
+	}
+
+	fmt.Println(query)
+	fmt.Println(args)
+
+	query += ";"
+
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	//turn the data into proper json
+	var athletes []vw_athletes
+	for rows.Next() {
+		var athlete vw_athletes
+		var NickName sql.NullString
+		var GymName sql.NullString
+		var Height sql.NullString
+		var Location sql.NullString
+		var Reach sql.NullInt64
+		var Dob sql.NullTime
+		var ActiveStatus sql.NullBool
+		var InstagramLink sql.NullString
+		var TwitterLink sql.NullString
+		var FacebookLink sql.NullString
+		var S3Url sql.NullString
+		if err := rows.Scan(
+			&athlete.Id,
+			&athlete.FirstName,
+			&athlete.LastName,
+			&NickName,
+			&GymName,
+			&Location,
+			&Height,
+			&Reach,
+			&Dob,
+			&ActiveStatus,
+				&athlete.PromotionId,
+				&InstagramLink,
+				&TwitterLink,
+				&FacebookLink,
+				&S3Url,
+			); err != nil {
+				http.Error(w, "Data extraction error", http.StatusInternalServerError)
 				fmt.Println(err)
 				return
 			}
+			athlete.NickName = utils.NullStringToString(NickName) 
+			athlete.GymName = utils.NullStringToString(GymName)
+			athlete.Height = utils.NullStringToString(Height)
+			athlete.Location = utils.NullStringToString(Location)
+			athlete.Reach = utils.NullInt64ToInt(Reach)
+			athlete.Dob = utils.NullTimeToTime(Dob)
+			athlete.ActiveStatus= utils.NullBoolToBool(ActiveStatus) 
+			athlete.InstagramLink = utils.NullStringToString(InstagramLink)
+			athlete.TwitterLink = utils.NullStringToString(TwitterLink)
+			athlete.FacebookLink = utils.NullStringToString(FacebookLink)
+			athlete.S3Url = utils.NullStringToString(S3Url)
+			athletes = append(athletes, athlete)
+		}
 
-			// Set the content type and write the JSON data
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(jsonData)
-		default:
-  			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
- 	}
+		//return data
+	jsonData, err := json.Marshal(athletes)
+	if err != nil {
+		http.Error(w, "JSON serialization failed", http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+
+	// Set the content type and write the JSON data
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
