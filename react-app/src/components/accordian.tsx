24ae927fb70d7e9c21225120c5af539e26826690
vietnguyen-dev <vietnguyent22@gmail.com>
2024-04-29 19:47:29 -0700

    need to add up and down arrow svgs

--- react-app/src/components/accordian.tsx
+++ react-app/src/components/accordian.tsx
@@ -8,14 +8,32 @@ interface iAccordianProps {
 	weightClasses: iWeightClass[],
 	rankings: iRankings[]
 }
+
+type WeightClassesExpandedState = Record<number, boolean>
 function Accordian({ weightClasses, rankings }: iAccordianProps) {
-	//create an object that controls the state of each weight class div, onclick => show the runner up panel
+	// Initialize state with default values (all collapsed)
+	const [isExpanded, setIsExpanded] = useState<WeightClassesExpandedState>(() => {
+		const initialState: WeightClassesExpandedState = {};
+		weightClasses.forEach((wc: iWeightClass) => {
+			initialState[wc.id] = true; // All sections start collapsed
+		});
+		return initialState;
+	});
+
+	// Function to toggle the state for a specific weight class
+	const toggleExpansion = (id: number) => {
+		setIsExpanded((prevState) => ({
+			...prevState,
+			[id]: !prevState[id], // Toggle the specific section
+		}));
+	};
 
 	return (
 		<>
-			{weightClasses.map((weightClass) => {
+			{weightClasses.map((weightClass: iWeightClass) => {
 				const ranked = rankings.filter((ranking) => ranking.weight_class_id == weightClass.id)
 				const champ = ranked[0]
+				const runnerUps = ranked.slice(1, ranked.length)
 				return (
 					<section key={weightClass.id} id={`weight-class-${weightClass.id}`}>
 						<div className="p-6 border-[1px] border-b-gray-300 flex justify-between">
@@ -28,23 +46,17 @@ function Accordian({ weightClasses, rankings }: iAccordianProps) {
 									: <h3 className="mb-1">VACANT</h3>}
 								<h4 className="text-xs text-gray-600">CHAMPION</h4>
 							</div>
-							<div>
-								<label className="swap swap-rotate">
-
-									{/* this hidden checkbox controls the state */}
-									<input type="checkbox" />
-
-									{/* sun icon */}
-									<svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
-
-									{/* moon icon */}
-									<svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
-
-								</label>
-							</div>
+							<button onClick={() => toggleExpansion(weightClass.id)}>{isExpanded[weightClass.id] ? <p>up</p> : <p>down</p>}</button>
 						</div>
-						<div className="px-3 hidden">
-							<h5>runner ups</h5>
+						<div className={` ${isExpanded[weightClass.id] && 'hidden'}`}>
+							{runnerUps.map((ranking: iRankings) =>
+								<div key={ranking.athlete_id} className="flex px-6 py-4 border-b-gray-300">
+									<h4 className="font-semibold text-sm text-gray-500 mr-3">{ranking.ranking}</h4>
+									<h4 className="font-bold text-sm">
+										<Link href={`/athletes/${ranking.athlete_id}`}>{ranking.athlete_name.toUpperCase()}</Link>
+									</h4>
+								</div>)}
+
 						</div>
 					</section >
 				);
